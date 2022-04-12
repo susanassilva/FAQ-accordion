953bf7d89551004d515485e02164f82af96d3f6a
Susana S Silva <susana_santos@id.uff.br>
2022-04-12 16:53:28 -0300

    'add script'

---
+++ script.js
@@ -0,0 +1,16 @@
+const accordion = document.getElementsByClassName('accordion');
+let i;
+
+for (let i =0; i < accordion.length; i++) {
+  accordion[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+
+
+  var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
